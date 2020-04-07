d03ba5415f4d9d0764fc84bbf030f045cc7fe233
jochen84 <simman@rocketmail.com>
2020-04-07 14:58:17 +0200

    Userlist fixed, shows assigned tasks User-view fixed, shows taskes Task-view shows assigned user or blank if unassigned
    Create-task started, needs @Validation on Deadline and reward.
    Localdate deadline works, but need so be "controlled"

--- src/main/java/se/ecutb/todoapplication/Seeder.java
+++ src/main/java/se/ecutb/todoapplication/Seeder.java
@@ -11,6 +11,7 @@ import se.ecutb.todoapplication.entity.AppUser;
 import se.ecutb.todoapplication.entity.AppUserRole;
 import se.ecutb.todoapplication.entity.Role;
 import se.ecutb.todoapplication.entity.TodoItem;
+import se.ecutb.todoapplication.service.TodoItemService;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDate;
@@ -26,6 +27,7 @@ public class Seeder {
     private AppUserRoleRepo appUserRoleRepo;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
     private TodoItemRepo todoItemRepo;
+    // private TodoItemService todoItemService; //Service vi ska använda? Inte repo?
 
     @Autowired
     public Seeder(AppUserRepo appUserRepo, AppUserRoleRepo appUserRoleRepo, BCryptPasswordEncoder bCryptPasswordEncoder, TodoItemRepo todoItemRepo) {
@@ -58,23 +60,11 @@ public class Seeder {
         //Skapa Todos för att lägga till användaren
         TodoItem washTodo = new TodoItem("Tvätta bilen", "Här behövs massvis med instruktioner som berättar hur man går tillväga för att tvätta sin bil!", LocalDate.of(2020,05,01), false, 50);
         TodoItem cleanTodo = new TodoItem("Städa garderoben", "Likadant här, finns inte en karl som kan göra det utan vettiga instruktioner från sin sambo/fru..", LocalDate.of(2020,06,05), false, 100);
-        Set<TodoItem> todoSet = new HashSet<>();
-        
-        // Detta funkar. save måste komma först.
-        todoItemRepo.save(cleanTodo);
-        todoItemRepo.save(washTodo);
-        todoSet.add(washTodo);
-        todoSet.add(cleanTodo);
 
-        // Får inte denna att koppla till användare... Kan vara ett cascade-problem.
+        newUser.addUsersTodo(cleanTodo);
+        newUser.addUsersTodo(washTodo);
 
-        appUserRepo.save(newUser);
-        cleanTodo.setAssignee(newUser);
-        washTodo.setAssignee(newUser);
-
-
-
-
+        newUser = appUserRepo.save(newUser);
 
     }
 
